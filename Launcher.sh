76d90e993348aea8838b1cd4346513c2dc3e64c4
Florian CHARRIAT <fcharriat@clust-slurm-client.ifb.local>
2020-08-31 14:01:14 +0200

    Ajout Additional files and launcher

---
+++ Launcher.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+module load snakemake/5.19.2 python/3.7
+snakemake -s annotation_pipeline.snake --jobs 200 --cluster '{cluster.scheduler} {cluster.queue} {cluster.export_env} {cluster.cwd} {cluster.mem} {cluster.n_cpu}{threads}' --cluster-config Additional_files/config_cluster_slurm.yaml --use-singularity $1 --singularity-args '--home ~/' | tee stdout.txt
